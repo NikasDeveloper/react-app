a8e1822542e7bc5ec7d6886981503360ce252258
damiano.barbati <damiano.barbati@gmail.com>
2017-07-25 21:30:46 +0200

    trans

--- apps/visitor/router.js
+++ apps/visitor/router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ConnectedRouter } from 'react-router-redux';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
+import { RouteTransition } from 'react-router-transition';
 
 import Nav from './components/nav';
 import Component1 from './components/component1';
@@ -11,13 +12,45 @@ export const history = createHistory({ basename: '/' });
 
 export default class Router extends React.Component {
     render() {
+        const fade = {
+            atEnter: { opacity: 0 },
+            atActive: { opacity: 1 },
+            atLeave: { opacity: 0 },
+        };
+
+        const slideLeft = {
+            atEnter: { translateX: 100 },
+            atActive: { translateX: 0 },
+            atLeave: { translateX: -100 },
+            mapStyles: styles => ({ transform: `translateX(${styles.translateX}%)` }),
+        };
+
+        const slideRight = {
+            atEnter: { translateX: 100 },
+            atActive: { translateX: 0 },
+            atLeave: { translateX: 100 },
+            mapStyles: styles => ({ transform: `translateX(${styles.translateX}%)` }),
+        };
+
         return (
             <ConnectedRouter history={history}>
-                <Switch>
-                    <Route exact path="/component1" component={Component1} />
-                    <Route exact path="/component2" component={Component2} />
-                    <Route exact path="*" component={Nav} />
-                </Switch>
+                <div id="container">
+                    <Nav />
+                    <Route render={ ({ location, history, match }) => {
+                        const { action } = history;
+                        const animation = action === 'PUSH' ? slideLeft : slideRight;
+                        console.log(action);
+
+                        return (
+                            <RouteTransition pathname={location.pathname} {...animation}>
+                                <Switch>
+                                    <Route exact path="/component1" component={Component1} />
+                                    <Route exact path="/component2" component={Component2} />
+                                </Switch>
+                            </RouteTransition>
+                        );
+                    }} />
+                </div>
             </ConnectedRouter>
         );
     }
