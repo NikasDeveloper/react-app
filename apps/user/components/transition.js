12b35a06563caddd0d8c1097537b3bd5d7684da6
damiano.barbati <damiano.barbati@gmail.com>
2017-08-14 15:34:35 +0200

    refactor

---
+++ apps/user/components/transition.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import autobind from 'autobind-decorator';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import { push } from 'react-router-redux';
+import { RouteTransition, presets } from 'react-router-transition';
+import injectSheet from 'react-jss'
+import cx from 'classnames';
+
+import Nav from './nav';
+
+@withRouter
+@injectSheet(theme => ({
+    transitionContainer: {
+        width: '100vw',
+    },
+    transitionedViewContainer: {
+        position: 'absolute',
+    },
+}))
+@connect((state, props) => ({ }))
+export default class Transition extends React.Component {
+    static propTypes = {
+        children: PropTypes.oneOfType([PropTypes.element, PropTypes.arrayOf(PropTypes.element)]).isRequired,
+        dispatch: PropTypes.func.isRequired,
+        location: PropTypes.object.isRequired,
+        match: PropTypes.object.isRequired,
+        history: PropTypes.object.isRequired,
+        classes: PropTypes.object.isRequired,
+    }
+
+    render () {
+        const { children, history, classes } = this.props;
+        const { action } = history;
+
+        const animation = action === 'PUSH' ? presets.slideLeft : presets.slideRight;
+
+        return (
+            <RouteTransition pathname={location.pathname} component={false} {...animation} runOnMount={false} className={cx(classes.transitionContainer)}>
+                <div className={cx(classes.transitionedViewContainer)}>
+                    {children}
+                </div>
+            </RouteTransition>
+        );
+    }
+}
